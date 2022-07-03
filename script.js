a08738619dcaba143ff7e646813ffc857584d39e
LazyShikazu <107610719+LazyShikazu@users.noreply.github.com>
2022-07-04 00:55:32 +0530

    enhanced but small doubt
    aa game condition tileclick function lo pedithe ne further clicks theskotledu,
    for loop lo pettina kuda theskokudadu kada ideally? nen first for loop lo aa condition rasthe game over ainappudu false aina click chesthe maruthundi, why is that?

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 var boardElement = document.getElementById("board");
 var currentPlayer = "O";
 var boardValues = [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']];
-var game = true;
+var game = true; // created a game variable
 for (let i = 0; i < 9; i++) {
   var tile = document.createElement("div");
   boardElement.appendChild(tile);
@@ -16,7 +16,7 @@ for (let i = 0; i < 9; i++) {
 
 
 function tileClick() {
-  if (game == true) {
+  if (game == true) { //game true unnantha sepe run ayettu condition raasa
     console.log(game + "hello");
     var x = this.id.charAt(0);
     var y = this.id.charAt(2);
@@ -36,7 +36,7 @@ function tileClick() {
     if (check()) {
       document.getElementById("whoswinner").innerText = (currentPlayer == 'X' ? 'O' : 'X');
       document.getElementById("winnerText").style = "opacity : 1";
-      game = false;
+      game = false; // game aypogane false chesthunna
     }
   }
 }
@@ -106,4 +106,11 @@ function check() {
 Enchancements
 1. Don't let user input anything once anyone wins the game
 vokay donee
+
+nak em artham kaledu ante for loop lopala i wrote a condition i<9 dhaggara (6th line)
+for(i<9 && game) ani rasa, i thought game false avvagane click chesina theskodu ala ankoni
+but for loop lo condition rasina click chesthe evnt theskuntundi
+click event function lo rayalsi vachindhi if game == true ani
+why is it like that? for loop run avvakapoina click endhuku theskuntundi?
+u are calling the event in the loop itself no?(11th line)
 */
